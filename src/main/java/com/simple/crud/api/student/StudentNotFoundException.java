a395bed2009585820840f4c7b0f5a4d0296270c0
sadeep <sadeeproshan22@gmail.com>
2020-10-08 23:29:05 +0530

    Adds student controller

---
+++ src/main/java/com/simple/crud/api/student/StudentNotFoundException.java
@@ -0,0 +1,9 @@
+package com.simple.crud.api.student;
+
+public class StudentNotFoundException extends RuntimeException {
+
+	public StudentNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
